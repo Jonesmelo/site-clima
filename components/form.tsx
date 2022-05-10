41ef38b4ba5f3639640bed8927ed8960fbcc6d43
Jonesmelo <jones_melo14@hotmail.com>
2022-05-10 17:30:09 -0300

    ajuste no form, removi o campo estado

--- components/form.tsx
+++ components/form.tsx
@@ -3,15 +3,12 @@ import styles from './form.module.css'
 export default function Form(): JSX.Element {
     return (
         <div>
-            <form action="/action_page.php">
+            <form>
                 <label htmlFor="fname">Nome da cidade </label>
-                <input type="text" id="fname" name="firstname" placeholder="Digite sua cidade.."></input>
-
-                <label htmlFor="lname">Nome do Estado (Ex: BA) </label>
-                <input type="text" id="lname" name="lastname" placeholder="Digite seu Estado.."></input>
+                <input type="text" id="cidade" name="cidade" placeholder="Digite sua cidade.."></input>
 
                 <label htmlFor="country">Selecione seu Estado: </label>
-                <select id="country" name="country">
+                <select id="estado" name="estado">
                     <option value="ba">Bahia</option>
                     <option value="sp">São Paulo</option>
                     <option value="rj">Rio de Janeiro</option>
